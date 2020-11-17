56dfdaa4612392695e5cec83673cd5a79781bd92
Andrew R Smith <63675982+AndrewCodesDontYaKnow@users.noreply.github.com>
2020-11-16 18:14:08 -0600

    added boilerplate app

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  console.log("app executed");
+
+  let x = 1;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hasdfasdfasdfjohfafafnny</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
