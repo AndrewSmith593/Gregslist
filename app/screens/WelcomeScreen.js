3404bd9ef5a5c75be2824dc19815a7fc943e09e0
Andrew R Smith <63675982+AndrewCodesDontYaKnow@users.noreply.github.com>
2020-11-23 17:18:03 -0600

    added view image screen

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { ImageBackground, StyleSheet, View } from 'react-native';
+import { Image, ImageBackground, StyleSheet, View, Text } from 'react-native';
 
 function WelcomeScreen(props) {
     return (
         <ImageBackground 
         source={require('../assets/background.jpg')} 
         style={styles.background}>
+            <View style={styles.logoContainer}>
+            <Image style={styles.logo} source={require('../assets/logo-red.png')} />
+            <Text>Sell the stuff you don't want!</Text>
+            </View>
             <View style={styles.loginButton}></View>
+            <View style={styles.registerButton}></View>
         </ImageBackground>
     );
 }
@@ -15,11 +20,27 @@ const styles = StyleSheet.create({
    background: {
        flex: 1,
        justifyContent: 'flex-end',
+       alignItems: 'center',
    } ,
    loginButton: {
        width: '100%',
        height: 70,
-       backgroundColor: '#fc5c65'
+       backgroundColor: '#fc5c65',
+   },
+   registerButton: {
+       width: '100%',
+       height: 70,
+       backgroundColor: '#4ecdc4',
+   },
+   logo: {
+       height: 100,
+       width: 100,
+       marginBottom: 10,
+    },
+    logoContainer: {        
+        position: 'absolute',
+        top: 70,
+        alignItems: 'center',
    }
 })
 
