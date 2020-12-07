4a0867eea25cd0a6d78e73029a5f21f760b29d5a
Andrew R Smith <63675982+AndrewCodesDontYaKnow@users.noreply.github.com>
2020-12-07 12:26:30 -0600

    added formik hooks to imageinputlist and scrollToEnd for image scrollView

--- App.js
+++ App.js
@@ -4,25 +4,8 @@ import Screen from "./app/components/Screen";
 import * as Permissions from "expo-permissions";
 import { Image, Button } from "react-native";
 import ImageInputList from "./app/components/ImageInputList";
+import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 export default function App() {
-  const [imageUris, setImageUris] = useState([]);
-
-const handleAdd = uri => {
-  setImageUris([...imageUris, uri]);
-}
-
-const handleRemove = uri => {
-  setImageUris(imageUris.filter(imageUri => imageUri !== uri))
-}
-
-  return (
-    <Screen>
-      <ImageInputList
-        imageUris={imageUris}
-        onAddImage={handleAdd}
-        onRemoveImage={handleRemove}
-      />
-    </Screen>
-  );
+  return <ListingEditScreen />
 }
--- app/components/ImageInputList.js
+++ app/components/ImageInputList.js
@@ -1,25 +1,33 @@
-import React from "react";
+import React, { useRef } from "react";
 import { View, StyleSheet, ScrollView } from "react-native";
 import ImageInput from "./ImageInput";
 
 function ImageInputList({ imageUris = [], onRemoveImage, onAddImage }) {
-  
-  
+  const scrollView = useRef();
+
   return (
-    <ScrollView horizontal>
-      <View style={styles.container}>
-        {imageUris.map((uri) => (
-          <View key={uri} style={styles.image}>
-            <ImageInput
-              imageUri={uri}
-              key={uri}
-              onChangeImage={() => onRemoveImage(uri)}
-            />
-          </View>
-        ))}
-        <ImageInput onChangeImage={(uri) => onAddImage(uri)} />
-      </View>
-    </ScrollView>
+    <View>
+      <ScrollView
+        ref={scrollView}
+        horizontal
+        onContentSizeChange={() => {
+          scrollView.current.scrollToEnd();
+        }}
+      >
+        <View style={styles.container}>
+          {imageUris.map((uri) => (
+            <View key={uri} style={styles.image}>
+              <ImageInput
+                imageUri={uri}
+                key={uri}
+                onChangeImage={() => onRemoveImage(uri)}
+              />
+            </View>
+          ))}
+          <ImageInput onChangeImage={(uri) => onAddImage(uri)} />
+        </View>
+      </ScrollView>
+    </View>
   );
 }
 
